cd8bfefd68aff747cb07e5f19d40fbba95a50daa
Yuyi <yuyi365@gmail.com>
2021-10-16 11:13:29 -0400

    updated

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    let text = document.querySelector('#text')
+    replaceText(text);
+});
+
+const replaceText = (e) => {
+    e.textContent = "This is really cool!";
+}
